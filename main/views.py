1b58b6cebbe8d52a1c788d85f2ef0d60faa2e783
lo2q <suhyeonkvvon@gmail.com>
2025-04-07 23:30:31 +0900

    2주차 과제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request) :
+     return render(request, 'main/mainpage.html');
+
+def tmi(request) :
+     return render(request, 'main/tmi.html');
